1162558573acb57a141113e35af9edfb48ec950d
mati <jmsalinas@uc.cl>
2018-12-02 23:23:23 -0300

    update

---
+++ assets/js/myscript.js
@@ -0,0 +1,27 @@
+var menuHideShow = $('.nav-menu-res').children(".menu-hide").hasClass('show');
+
+	function desplazar (){
+		$('.menu-hide').toggleClass('show');
+		$('#hamburger').toggleClass('hamburguer-slide');
+	}
+
+	function esconder (e){		
+		$('.menu-hide').removeClass('show');
+		$('#hamburger').removeClass('hamburguer-slide');
+	}
+
+	$('#hamburger').on('click', function(e){		
+		desplazar();
+		e.stopPropagation();
+		menuHideShow = $('.nav-menu-res').children(".menu-hide").hasClass('show');
+		console.log(menuHideShow)
+	})
+	
+   
+	window.addEventListener('click', function(e){
+		// var menuHide = $('.menu-hide');
+		if (menuHideShow === true && e.target.className != 'menu-hide show') {
+			desplazar();			
+		}		
+		console.log(e.target)
+	})
